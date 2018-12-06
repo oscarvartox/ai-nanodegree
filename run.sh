3d380ad3b4e1e38b5313398f589e387d40b68bf5
Oscar Vargas Torres <oscarvarto@protonmail.com>
2018-12-06 10:15:52 -0600

    Small fix to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -it --rm \
-    -p 0.0.0.0:4000:4000 \
+    -p 4000:4000 \
     --network=host \
     --name=scaffold \
     -v ~/.ivy2:/root/.ivy2  -v ~/.sbt:/root/.sbt \
